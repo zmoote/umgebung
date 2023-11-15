efd37ff172c04398b8fdd26f2d124a1f27d613df
Zachary Moote <nuluumo@gmail.com>
2023-11-14 19:25:54 -0500

    Wait to add CUDA

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <ctime>
+#include "../include/umgebung/Multiverse.h"
+#include "../include/umgebung/Logger.h"
+
+int main() 
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
